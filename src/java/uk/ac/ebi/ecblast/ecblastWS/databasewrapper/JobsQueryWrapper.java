9862d1938a98d43a60a60d0cc26adc020b10797d
Syed Asad Rahman <s9asad@gmail.com>
2014-03-11 12:54:04 +0000

    disused imports removed

--- src/java/uk/ac/ebi/ecblast/ecblastWS/databasewrapper/JobsQueryWrapper.java
+++ src/java/uk/ac/ebi/ecblast/ecblastWS/databasewrapper/JobsQueryWrapper.java
@@ -12,7 +12,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.catalina.tribes.transport.RxTaskPool;
 
 /**
  *
